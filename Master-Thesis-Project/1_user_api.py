92168fc93fc716e0ef7173653e5e05c8879c5b83
Mohammed Zoher Guniem <mghunime@yahoo.no>
2021-04-13 12:33:06 +0200

    added a valid graph check on user api to make sure both graphs are available from a certain crawl

--- Master-Thesis-Project/1_user_api.py
+++ Master-Thesis-Project/1_user_api.py
@@ -72,7 +72,11 @@ def constructJSGraph(neo4j_graph, graph_type, score_type, centrality_max):
 def index():
     user_graphs = neo4j_users_db_connector.get_graphs()
     activity_graphs = neo4j_activities_db_connector.get_graphs()
-    return render_template("index.html", user_graphs=user_graphs, activity_graphs=activity_graphs)
+    valid_graphs = []
+    for graph in user_graphs:
+        if graph in activity_graphs:
+            valid_graphs.append(graph)
+    return render_template("index.html", graphs=valid_graphs)
 
 
 # Example - GUI: http://localhost:5000/user_graph?graph=Test_2021-04-11&score_type=total&centrality=betweenness
